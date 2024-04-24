b70dcea137eb2427ccbd4784f1d5d193d6c29510
Orik25 <orestpilavka@gmail.com>
2024-04-24 22:35:34 +0300

    init commit

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/home")]
+    public class HomeController : Controller
+    {
+        [Route("page")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
