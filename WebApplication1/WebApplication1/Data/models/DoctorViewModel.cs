e000f4dacf78243331024d60283008e22a66081b
Danylo77 <dankoiniesta@gmail.com>
2024-05-09 00:43:06 +0300

    make: add modals for patient

---
+++ WebApplication1/WebApplication1/Data/models/DoctorViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Data.models
+{
+    public class DoctorViewModel
+    {
+        private EF.User user;
+
+        public List<Appoint> Appointments { get; set; }
+
+        public DoctorViewModel(List<Appoint> appointments, EF.User user)
+        {
+            Appointments = appointments;
+            this.user = user;
+        }
+    }
+}
